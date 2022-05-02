660928405b2fe546182dddbde159667a37ec29fc
Gabriel Zúñiga Orozco <GABRIEL.ZUNIGAOROZCO@ucr.ac.cr>
2022-05-01 21:43:54 -0600

    Creado SongController.cs, SongModel.cs, Index.cshtml en una carpeta Song, y finalmente editado _Layout.cshtml, para crear una sección con información sobre una canción

---
+++ Laboratorio3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorio3.Models;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var song = GetSongData();
+            ViewBag.MainTitle = "My favorite song";
+            return View(song);
+        }
+
+        private SongModel GetSongData()
+        {
+            SongModel song = new SongModel();
+            song.Name = "Mikazuki";
+            song.Artist = "Sayuri";
+            song.Genre = "J-pop";
+            song.Duration = "4:21";
+            song.ReleasedDate = new DateTime(2017, 05, 17);
+
+            return song;
+        }
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public string Duration { get; set; }
+        public DateTime ReleasedDate { get; set; }
+    }
+}
