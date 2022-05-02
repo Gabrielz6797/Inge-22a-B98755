8194521901f85117c0a997b282a73a27d127e5b1
Gabriel Zúñiga Orozco <GABRIEL.ZUNIGAOROZCO@ucr.ac.cr>
2022-05-01 19:46:34 -0600

    Modificado MovieController.cs e Index.cshtml para mostrar lista de películas

--- Laboratorio3/Laboratorio3/Controllers/MovieController.cs
+++ Laboratorio3/Laboratorio3/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorio3.Models;
 
 namespace Laboratorio3.Controllers
 {
@@ -6,7 +7,37 @@ namespace Laboratorio3.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var movies = GetListOfMovies();
+            ViewBag.MainTitle = "List of my favorite films";
+            return View(movies);
+        }
+
+        private List<MovieModel> GetListOfMovies()
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            movies.Add(new MovieModel
+            {
+                Id = 1,
+                Name = "Pulp Fiction",
+                Genre = "Crime/Drama",
+                ReleasedDate = new DateTime(1994, 10, 14)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 2,
+                Name = "Toy Story",
+                Genre = "Family/Comedy",
+                ReleasedDate = new DateTime(1995, 11, 22)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 3,
+                Name = "Mulan",
+                Genre = "Family/Comedy",
+                ReleasedDate = new DateTime(1998, 06, 19)
+            });
+
+            return movies;
         }
     }
 }
