ed55ad84b80752c479215edc49351544493b9c26
Gabriel Zúñiga Orozco <GABRIEL.ZUNIGAOROZCO@ucr.ac.cr>
2022-07-26 18:46:12 -0600

    Realizada configuración inicial

--- Examen2-B98755/examen2-b98755/src/setupProxy.js
+++ Examen2-B98755/examen2-b98755/src/setupProxy.js
@@ -6,7 +6,7 @@ const context = [
 
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
-        target: 'https://localhost:5001',
+        target: 'http://localhost:5205',
         secure: false
     });
 
